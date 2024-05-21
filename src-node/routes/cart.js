7932eaf55570d2aecf4a8fb95904deb014cd29bd
Quentin <quentinmet7@gmail.com>
2024-05-21 10:35:57 +0200

    Fin du cours 21.05.2024

--- src-node/routes/cart.js
+++ src-node/routes/cart.js
@@ -41,50 +41,62 @@ cartRouter.post(
       return res.status(400).json({ error: "Quantity must be a positive number" })
     }
 
+    if(cart[0].status == "checked out") {
+      return res.status(400).json({ error: "Cart is already checked out" })
+    }
+
     await executeQuery("INSERT INTO cart_item(cart_id, product_id, quantity) VALUES (?, ?, ?)", [cartId, productId, quantity])
 
-    res.status(201).json({ error: "Action is successfull" });
+    res.status(201).json({ message: "Action is successfull" });
   })
 );
 
 cartRouter.put(
-  "/:cardId/items/:productId", 
+  "/:cartId/items/:productId", 
   handleAsync(async (req, res, next) => {
-    const results = await executeQuery(
-      "SELECT * FROM cart_item WHERE id=?",
-      [req.params.productId]
-    );
+    const cart = await executeQuery("SELECT * FROM cart WHERE id=?;", [req.params.cartId])
+    const product = await executeQuery("SELECT * FROM product WHERE id=?", [req.params.productId])
 
     if(req.body.quantity < 1) {
       return res.status(400).json({ error: "Quantity must be a positive number" });
     }
 
-    console.log(results)
-
-    if(results == '') {
+    if(product == '') {
       return res.status(404).json({ error: `Product with ID ${req.params.productId} not found in cart` });
+    }
+
+    if(cart == '') {
+      return res.status(404).json({ error: "Cart not found" })
+    }
+
+    if(cart[0].status == "checked out") {
+      return res.status(400).json({ error: "Cart is already checked out" })
     }
 
     await executeQuery(
       "UPDATE cart_item SET quantity = ? WHERE cart_id=? AND product_id=?;",[req.body.quantity, req.params.cartId, req.params.productId]
     );
 
-    res.status(200).json({ error: "Action is successfull"})
+    res.status(200).json({ message: "Action is successfull"})
   })
 )
  
 cartRouter.delete(
-  "/:cardId/items/:productId",
+  "/:cartId/items/:productId",
   handleAsync(async (req, res, next) => {
     const product = await executeQuery("SELECT * FROM product WHERE id=?;", [req.params.productId]);
-    const cart = await executeQuery("SELECT * FROM cart WHERE id=?", [req.params.cartId]);
+    const cart = await executeQuery("SELECT * FROM cart WHERE id=?;", [req.params.cartId])
 
-    if(cart == "" || product == "") {
-      return res.status(404).json({ error: "ABC" });
+    if(product == "") {
+      return res.status(404).json({ error:  `Product with ID ${req.params.productId} not found in cart` })
     }
 
-    if(cart[0].status == "checkout") {
-      return res.status(404).json({ error: "ABC" });
+    if(cart == '') {
+      return res.status(404).json({ error: "Cart not found"})
+    }
+
+    if(cart[0].status == "checked out") {
+      return res.status(400).json({ error: "Cart is already checked out" })
     }
 
     await executeQuery(
@@ -92,21 +104,28 @@ cartRouter.delete(
       [req.params.productId]
     );
 
-    res.status(200).json({ error: "Action is sucessfull" })
+    res.status(200).json({ message: "Action is sucessfull" })
   })
 );
 
 cartRouter.get(
   "/:id/",
   handleAsync(async (req, res, next) => {
-    const cart = await executeQuery("SELECT * FROM `cart` JOIN cart_item ON cart.id = cart_item.cart_id JOIN product ON product.id = cart_item.product_id JOIN discount ON discount.product_id = product.id WHERE cart_item.id = ?;", [req.params.id])
+    const cart = await executeQuery("SELECT * FROM cart JOIN cart_item ON cart.id = cart_item.cart_id JOIN product ON product.id = cart_item.product_id JOIN discount ON discount.product_id = product.id WHERE cart_id = ?;", [req.params.id])
 
+    console.log(cart)
+
+    
+    if(cart == '') {
+      return res.status(404).json({ error: "Cart not found" })
+    }
+    
+    if(cart.status == "checked out") {
+      return res.status(200).json({ message: "Cart not found" })
+    }
+    
     let price = 0;
     let price2 = 0;
-
-    if(cart == '') {
-      return res.status(404).json({ error: "ABC" });
-    }
 
     cart.forEach((item) => {
       price += item.quantity * item.price
@@ -121,10 +140,7 @@ cartRouter.get(
       }
     });
 
-   // res.status(200).json({ cart, discountedPrice, price })
-
-
-   res.status(200).json({ cart, originalPrice: price, discountedPrice: price2 })
+    res.status(200).json({ "cartId": cart[0].cart_id, "status": cart[0].status, "items": cart, "originalPrice": price, "discountedPrice": price2 })
   })
 )
 
@@ -133,13 +149,19 @@ cartRouter.post(
   handleAsync(async (req, res, next) => {
     const cartId = req.params.id;
 
-    const card = await executeQuery("SELECT * FROM cart WHERE id = ?;", [cartId])
+    const cart = await executeQuery("SELECT * FROM cart WHERE id=?;", [cartId])
 
-    if(card == '') {
-      return res.status(404).json({ error: "ABC" });
+    if(cart == '') {
+      return res.status(404).json({ error: "Cart not found" })
     }
 
-    await executeQuery("UPDATE cart SET status = ? WHERE id = ?", ["checkout", cartId])
+    if(cart[0].status == "checked out") {
+      return res.status(400).json({ error: "Cart is already checked out" })
+    }
+
+    await executeQuery("UPDATE cart SET status=? WHERE id=?", ["checked out", cartId])
+
+    res.status(200).json({ message: "Checkout successful. Order placed."})
   })
 )
 
